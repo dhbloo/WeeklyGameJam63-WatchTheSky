4c6b065f7b2cbb5db1e0ea788a3583b906ab0d9a
NightHz <32999626+nighthz@users.noreply.github.com>
2018-09-24 21:06:28 +0800

    添加摄像机和角色移动

---
+++ WatchTheSky/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    public float sensitivityLeftRight = 1.0f;
+    public float sensitivityUpDown = 1.0f;
+
+    public float maxElevationAngle = 80.0f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        transform.position = player.transform.position;
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * sensitivityLeftRight -
+            transform.right * Input.GetAxis("Mouse Y") * sensitivityUpDown, Space.World);
+        
+    }
+
+}
---
+++ WatchTheSky/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public new GameObject camera;
+
+    public float speed = 1.0f;
+
+    private void Update()
+    {
+        Vector3 right = camera.transform.right;
+        Vector3 forward = Vector3.Cross(Vector3.up, right);
+        transform.position += (-forward * Input.GetAxis("Vertical") + right * Input.GetAxis("Horizontal")) * speed;
+    }
+}
