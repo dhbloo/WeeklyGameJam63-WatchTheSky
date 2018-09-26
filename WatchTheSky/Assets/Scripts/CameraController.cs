0ad3867c906c62abfc7ad76ee2fd6f620365adda
NightHz <32999626+nighthz@users.noreply.github.com>
2018-09-26 23:08:29 +0800

    添加 相机仰角限制

--- WatchTheSky/Assets/Scripts/CameraController.cs
+++ WatchTheSky/Assets/Scripts/CameraController.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
 
-    public float sensitivityLeftRight = 1.0f;
-    public float sensitivityUpDown = 1.0f;
-
-    public float maxElevationAngle = 80.0f;
+    public float maxElevationAngle = 80f;
+    public float minElevationAngle = -80f;
 
     public float eyeOffset = 0.6f;
 
@@ -22,9 +21,29 @@ public class CameraController : MonoBehaviour
     {
         transform.position = player.transform.position + Vector3.up * eyeOffset;
 
-        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * sensitivityLeftRight -
-            transform.right * Input.GetAxis("Mouse Y") * sensitivityUpDown, Space.World);
-        
+        float rotationAngleX = Input.GetAxis("Mouse X");
+        float rotationAngleY = Input.GetAxis("Mouse Y");
+
+        float nowElevationAngle = 90f - Vector3.SignedAngle(Vector3.up, transform.forward, transform.right);
+
+        float MaxRotationAngleY = maxElevationAngle - nowElevationAngle;
+        float MinRotationAngleY = minElevationAngle - nowElevationAngle;
+
+        rotationAngleY = Mathf.Clamp(rotationAngleY, MinRotationAngleY, MaxRotationAngleY);
+
+        transform.Rotate(Vector3.up * rotationAngleX - transform.right * rotationAngleY, Space.World);
+
     }
 
+}
+
+[CustomEditor(typeof(CameraController))]
+public class CameraControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        GUILayout.TextArea("Info : Open 'Edit->Project Settings->Input' to adjust sensitivity of MouseX and MouseY");
+    }
 }
