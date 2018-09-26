0d4598373e52e20dc5c41f167cf0f6a2b52ad43f
dhb <1084714805@qq.com>
2018-09-26 23:46:40 +0800

    更新云的移动

--- WatchTheSky/Assets/Scripts/CloudBehaviour.cs
+++ WatchTheSky/Assets/Scripts/CloudBehaviour.cs
@@ -7,14 +7,27 @@ public class CloudBehaviour : MonoBehaviour {
     public float MovingSpeedMin = 2.0f;
     public float MovingSpeedMax = 6.0f;
 
+    public float Mass = 20.0f;
+
     float movingSpeed;
+    bool floating;
+    public bool Floating { get { return floating; } }
 
     void Start () {
         movingSpeed = Mathf.Lerp(MovingSpeedMin, MovingSpeedMax, Random.value);
+        floating = true;
     }
 	
 	void Update () {
-        Transform t = transform;
-        t.localPosition = t.localPosition + (-t.localPosition).normalized * movingSpeed * Time.deltaTime;
+        if (floating) {
+            Transform t = transform;
+            t.localPosition = t.localPosition + (-t.localPosition).normalized * movingSpeed * Time.deltaTime;
+        }
+    }
+
+    public void Drop() {
+        floating = false;
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.mass = Mass;
     }
 }
--- WatchTheSky/Assets/Scripts/CloudShooter.cs
+++ WatchTheSky/Assets/Scripts/CloudShooter.cs
@@ -14,13 +14,12 @@ public class CloudShooter : MonoBehaviour {
         RaycastHit raycastHit;
         if (Physics.Raycast(Camera.position, Camera.forward, out raycastHit)) {
             GameObject hittedObject = raycastHit.collider.gameObject;
-            if (hittedObject.tag == "cloud" && hittedObject.GetComponent<Rigidbody>() == null) {
+            if (hittedObject.CompareTag("cloud") && hittedObject.GetComponent<CloudBehaviour>().Floating) {
                 // TODO : 加上高亮
                 hittedObject.transform.localScale = new Vector3(2, 2, 2);
 
-                if (Input.GetMouseButtonDown(0)) {
-                    hittedObject.AddComponent<Rigidbody>();
-                }
+                if (Input.GetMouseButtonDown(0))
+                    hittedObject.GetComponent<CloudBehaviour>().Drop();
             }
         }
 	}
