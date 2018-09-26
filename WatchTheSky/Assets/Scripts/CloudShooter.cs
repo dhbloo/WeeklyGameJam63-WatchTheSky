15d5a8f036ea2dec60c71ad77694f231d2fa7149
dhb <1084714805@qq.com>
2018-09-26 15:29:40 +0800

    Added shooting

---
+++ WatchTheSky/Assets/Scripts/CloudShooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudShooter : MonoBehaviour {
+
+    public Transform Camera;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        RaycastHit raycastHit;
+        if (Physics.Raycast(Camera.position, Camera.forward, out raycastHit)) {
+            GameObject hittedObject = raycastHit.collider.gameObject;
+            if (hittedObject.tag == "cloud" && hittedObject.GetComponent<Rigidbody>() == null) {
+                // TODO : 加上高亮
+                hittedObject.transform.localScale = new Vector3(2, 2, 2);
+
+                if (Input.GetMouseButtonDown(0)) {
+                    hittedObject.AddComponent<Rigidbody>();
+                }
+            }
+        }
+	}
+}
