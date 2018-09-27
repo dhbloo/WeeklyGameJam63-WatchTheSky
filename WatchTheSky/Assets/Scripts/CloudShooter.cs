ec0cef90e35e57ef9223a3a3ce00298110765760
dhb <1084714805@qq.com>
2018-09-27 22:56:10 +0800

    加入选择光柱

--- WatchTheSky/Assets/Scripts/CloudShooter.cs
+++ WatchTheSky/Assets/Scripts/CloudShooter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CloudShooter : MonoBehaviour {
 
     public Transform Camera;
+    public GameObject LightBar;
 
 	void Start () {
 		
@@ -12,10 +13,14 @@ public class CloudShooter : MonoBehaviour {
 	
 	void Update () {
         RaycastHit raycastHit;
+        LightBar.SetActive(false);
         if (Physics.Raycast(Camera.position, Camera.forward, out raycastHit)) {
             GameObject hittedObject = raycastHit.collider.gameObject;
             if (hittedObject.CompareTag("cloud") && hittedObject.GetComponent<CloudBehaviour>().Floating) {
                 hittedObject.GetComponent<CloudBehaviour>().HighLight();
+
+                LightBar.SetActive(true);
+                LightBar.transform.position = hittedObject.transform.position;
 
                 if (Input.GetMouseButtonDown(0))
                     hittedObject.GetComponent<CloudBehaviour>().Drop();
